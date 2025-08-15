f36b20c9307d8096ec90dbb68b21725dac5391ec
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2025-08-15 15:43:23 -0500

    Updated README and corrected substring logic by using the include method instead of a comparison operator

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sub_strings
+
+This is a Ruby program that is designed to find and count substrings within a given array regardless of its case. This project was created to showcase my understanding of substring analysis and string manipulation techniques in Ruby.
--- ruby.rb
+++ ruby.rb
@@ -15,7 +15,7 @@ def substrings(str, array)
   # Iterate over the given array and for each value count the amount of times a word appears from the split_words array appears
   array.each do |val|
     split_words.each do |word|
-     if word == val
+     if word.include?(val)
        temp_hash[val] += 1
      end
     end
@@ -26,7 +26,4 @@ def substrings(str, array)
 end
 
 substrings("Howdy partner, sit down! How's it going?", dictionary)
-
-# exmaple output
-# => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
-# => { "below" => 1, "low" => 1 }
+substrings("below", dictionary)

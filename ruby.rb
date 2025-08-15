4552173e1dc5b389152cfec413f217c00592b2ed
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2025-08-15 14:56:04 -0500

    Add launch configuration for debugging and started refactoring substrings method

--- ruby.rb
+++ ruby.rb
@@ -1,18 +1,35 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit","below"]
 
-
 def substrings(word, array)
   temp_hash = Hash.new(0)
 
-  array.each do |val|
-    if val == word
-      temp_hash[val] += 1
+  #split string into multiple words?
+  word.downcase!
+  print word;
+  
+  word.scan(/\w+/) do |word|
+    if array.include?(word)
+      temp_hash[word] += 1
     end
+    print temp_hash
   end
 
-  print temp_hash[word]
-
+=begin
+  #iterate through the dictionary array and compare it to the temp array to see if theres a match
+  array.each do |val|
+    temp_array.each do |temp_array_val|
+     if temp_array_val == val
+       temp_hash[val] += 1
+     end
+   end
+   print temp_hash
+  end
+=end
 end
 
+#make this word with multiple words?
+substrings("Howdy partner, sit down! How's it going?", dictionary)
 
-substrings("below", dictionary)
+# exmaple output
+# => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+# => { "below" => 1, "low" => 1 }
